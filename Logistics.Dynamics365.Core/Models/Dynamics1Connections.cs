58cd60fff226231681d9c79eae0490ff56a6313d
matheusmelgaco <matheusmelgacob@gmail.com>
2024-02-29 15:06:29 -0300

    Utilizando viaCEP para cotação e tentativa azure function

---
+++ Logistics.Dynamics365.Core/Models/Dynamics1Connections.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Linq;
+
+namespace Logistics.Dynamics365.Core.Models
+{
+    public static class Dynamics1Connections
+    {
+        public static CrmServiceClient GetEnvironment1()
+        {
+            // Aqui, você usaria as configurações diretamente, já que não temos um serviço IOrganizationService pré-existente.
+            // Supondo que você tenha as informações de URL, usuário e senha disponíveis para se conectar ao Dynamics 365.
+            string url = "https://org51c55b59.crm2.dynamics.com/"; // Substitua pela URL real do seu Dynamics 365
+            string user = "GlobalLogistics@LogisticsTCC.onmicrosoft.com"; // Substitua pelo usuário
+            string password = "Equipe4tcc@"; // Substitua pela senha
+
+            CrmServiceClient crmServiceClient = new CrmServiceClient($"AuthType=Office365; Url={url}; Username={user}; Password={password}");
+            return crmServiceClient;
+        }
+    }
+}
--- Logistics.Dynamics365.Customizações/WebResource Dynamics 1/Logistics.Dynamics365.Cotacao.js
+++ Logistics.Dynamics365.Customizações/WebResource Dynamics 1/Logistics.Dynamics365.Cotacao.js
@@ -1,63 +1,81 @@
 ﻿if (typeof (Logistics) === "undefined") Logistics = {};
 if (typeof (Logistics.Cotacao) === "undefined") Logistics.Cotacao = {};
 
-Logistics.Cotacao.OnChangeCEPBilling = function (context) {
-    Logistics.Cotacao.OnChangeCEP(context, true);
-};
+Logistics.Cotacao = {
+    isOnLoadComplete: false,
 
-Logistics.Cotacao.OnChangeCEPShipping = function (context) {
-    Logistics.Cotacao.OnChangeCEP(context, false);
-};
+    consultarCEP: function (cepValue, formContext, isBillingAddress) {
+        console.log("Consultando CEP: ", cepValue, " IsBillingAddress: ", isBillingAddress);
+        var globalContext = Xrm.Utility.getGlobalContext();
+        var serverURL = globalContext.getClientUrl();
+        var actionName = "lgs_ConsultaCep";
 
-Logistics.Cotacao.OnChangeCEP = function (context, isBillingAddress) {
-    const formContext = context.getFormContext();
-    const cepFieldName = isBillingAddress ? "lgs_cep" : "lgs_cep1";
-    const cepField = formContext.getAttribute(cepFieldName);
-    let cepValue = cepField.getValue();
+        var req = new XMLHttpRequest();
+        req.open("POST", serverURL + "/api/data/v9.2/" + actionName, true);
+        req.setRequestHeader("OData-MaxVersion", "4.0");
+        req.setRequestHeader("OData-Version", "4.0");
+        req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        req.setRequestHeader("Accept", "application/json");
 
-    if (cepValue) {
-        cepValue = cepValue.replace("-", "");
-        const validCepNumbers = /^(([0-9]{8}))$/;
+        req.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                req.onreadystatechange = null;
+                if (this.status === 200 || this.status === 204) {
+                    var result = JSON.parse(this.response);
+                    console.log("Resposta completa do servidor:", result);
 
-        if (validCepNumbers.test(cepValue)) {
-            var globalContext = Xrm.Utility.getGlobalContext();
-            var serverURL = globalContext.getClientUrl();
-            var actionName = "lgs_ConsultaCep";
+                    // Usando formatCep para o CEP formatado
+                    var formattedCep = result["formatCep"];
+                    console.log("Atualizando campo CEP com:", formattedCep);
+                    var fieldPrefix = isBillingAddress ? "billto_" : "shipto_";
 
-            var req = new XMLHttpRequest();
-            req.open("POST", serverURL + "/api/data/v9.2/" + actionName, true);
-            req.setRequestHeader("OData-MaxVersion", "4.0");
-            req.setRequestHeader("OData-Version", "4.0");
-            req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-            req.setRequestHeader("Accept", "application/json");
-
-            req.onreadystatechange = function () {
-                if (this.readyState === 4) {
-                    req.onreadystatechange = null;
-                    if (this.status === 200 || this.status === 204) {
-                        var result = JSON.parse(this.response);
-
-                        var formattedCep = result["cep"].substr(0, 5) + "-" + result["cep"].substr(5, 3);
-
-                        const fieldPrefix = isBillingAddress ? "billto_" : "shipto_";
-
-                       
-                        cepField.setValue(formattedCep);
-                        formContext.getAttribute(fieldPrefix + "line1").setValue(result["rua"]);
-                        formContext.getAttribute(fieldPrefix + "line2").setValue(result["bairro"]);
-                        formContext.getAttribute(fieldPrefix + "city").setValue(result["cidade"]);
-                        formContext.getAttribute(fieldPrefix + "stateorprovince").setValue(result["estado"]);
-                        formContext.getAttribute(fieldPrefix + "country").setValue("Brasil");
-
-                    } else {
-                        Xrm.Utility.alertDialog("Não foi possível consultar o CEP. Por favor, tente novamente.");
-                    }
+                    // Ajuste para usar os nomes corretos das propriedades da resposta
+                    formContext.getAttribute(isBillingAddress ? "lgs_cep" : "lgs_cep1").setValue(formattedCep);
+                    formContext.getAttribute(fieldPrefix + "line1").setValue(result["rua"]);
+                    formContext.getAttribute(fieldPrefix + "line2").setValue(result["bairro"]);
+                    formContext.getAttribute(fieldPrefix + "city").setValue(result["cidade"]);
+                    formContext.getAttribute(fieldPrefix + "stateorprovince").setValue(result["estado"]);
+                    formContext.getAttribute(fieldPrefix + "country").setValue("Brasil");
+                } else {
+                    console.error("Falha na requisição AJAX, status: ", this.status);
+                    Xrm.Utility.alertDialog("Não foi possível consultar o CEP. Por favor, tente novamente.");
                 }
-            };
-            req.send(JSON.stringify({ cep: cepValue }));
-        } else {
-            Xrm.Utility.alertDialog("CEP inválido! Insira um CEP válido.");
-            cepField.setValue(null);
+            }
+        };
+        req.send(JSON.stringify({ cep: cepValue }));
+    },
+
+    OnChangeCEPShipping: function (context) {
+        console.log("OnChangeCEPShipping chamado");
+        var formContext = context.getFormContext();
+        var cepField = formContext.getAttribute("lgs_cep1");
+        var cepValue = cepField.getValue();
+
+        if (cepValue) {
+            cepValue = cepValue.replace("-", "");
+            if (/^([0-9]{8})$/.test(cepValue)) {
+                this.consultarCEP(cepValue, formContext, false);
+            } else {
+                Xrm.Utility.alertDialog("CEP inválido! Insira um CEP válido.");
+                cepField.setValue(null);
+            }
+        }
+    },
+
+    OnChangeCEPBilling: function (context) {
+        console.log("OnChangeCEPBilling chamado");
+        var formContext = context.getFormContext();
+        var cepField = formContext.getAttribute("lgs_cep");
+        var cepValue = cepField.getValue();
+
+        if (cepValue) {
+            cepValue = cepValue.replace("-", "");
+            if (/^([0-9]{8})$/.test(cepValue)) {
+                this.consultarCEP(cepValue, formContext, true);
+            } else {
+                Xrm.Utility.alertDialog("CEP inválido! Insira um CEP válido.");
+                cepField.setValue(null);
+            }
         }
     }
 };
